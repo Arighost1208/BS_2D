2d82f1c604f3c96a959fd3f914a173b709bd2022
LucasFeraud <85590431+Peraud@users.noreply.github.com>
2022-06-13 10:54:09 -0300

    Se agregaron personajes

---
+++ Assets/Scripts/SoundButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundButton : MonoBehaviour
+{
+
+    public AudioSource font;
+    public AudioClip clip;
+
+   
+    void Start()
+    {
+        font.clip = clip;
+    }
+
+    public void PlayClip()
+    {
+        font.Play();
+    }
+
+   
+}
---
+++ Assets/Scripts/VolumeGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeGame : MonoBehaviour
+{
+
+    public Slider slider;
+    public float sliderValue;
+    //public Image imageMute;
+
+    
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("VolumeAudio", 0.5f);
+        AudioListener.volume = slider.value;
+        //Ismutetrue();
+        
+    }
+
+    public void ChangeSlider(float value)
+    {
+        sliderValue = value;
+        PlayerPrefs.SetFloat("VolumenAudio", sliderValue);
+        AudioListener.volume = slider.value;
+        //Ismutetrue();
+    }
+
+    //public void Ismutetrue()
+    //{
+    //    if(sliderValue == 0)
+    //    {
+    //        imageMute.enabled = true;
+    //    }
+    //    else
+    //    {
+    //        imageMute.enabled = false;
+    //    }
+    //}
+}
