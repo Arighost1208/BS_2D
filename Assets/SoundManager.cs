8116dc6e80ac60d24987da9f1ab6309930d0e1fc
LucasFeraud <85590431+Peraud@users.noreply.github.com>
2022-06-16 19:37:03 -0300

    Menu y extras

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static AudioSource[] _srcs;
+
+    private void Awake()
+    {
+        _srcs = GetComponents<AudioSource>();
+    }
+
+    public static void PlayClip(AudioClip clip, int source = 0)
+    {
+        PlayClip(clip, 1f, source);
+    }
+
+    public static void PlayClip(AudioClip clip, float volume, int source = 0)
+    {
+        if (clip == null) return;
+        _srcs[source].volume = volume;
+        _srcs[source].PlayOneShot(clip);
+    }
+}
